21a231678572cbc0037973278c805c78f93fe733
takaman <takaman@gmail.com>
2022-05-16 21:26:30 +0300

    Update entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -4,24 +4,6 @@
 # non-zero status
 set -e
 
-# xtrace: Print a trace of simple commands, `for` commands, `case` commands,
-# `select` commands, and arithmetic `for` commands and their arguments or
-# associated word lists after they are expanded and before they are executed.
-set -x
-
-if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
-  echo "Saving SSH_PRIVATE_KEY"
-
-  mkdir -p /root/.ssh
-  echo "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
-  chmod 600 /root/.ssh/id_rsa
-
-  # Github action changes $HOME to /github at runtime
-  # therefore we always copy the SSH key to $HOME (aka. ~)
-  mkdir -p ~/.ssh
-  cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true
-fi
-
 BASE_REF=$1
 HEAD_BRANCHES=$2
 
@@ -47,11 +29,6 @@ echo "HEAD_BRANCHES=${HEAD_BRANCHES}"
 mkdir _tmp && cd _tmp
 git init
 
-# git config user.name "${GITHUB_ACTOR}"
-# git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
-git remote add origin "https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git remote set-url origin "https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git remote -v
 git remote update
