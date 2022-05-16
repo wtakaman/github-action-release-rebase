f5d51e128f0ab89b18d75f39e0569751723f54e3
takaman <takaman@gmail.com>
2022-05-16 20:42:45 +0300

    Update entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -50,8 +50,8 @@ git init
 # git config user.name "${GITHUB_ACTOR}"
 # git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git remote add origin "https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git remote set-url origin "https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git remote -v
 git remote update
