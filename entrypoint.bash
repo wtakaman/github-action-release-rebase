9401b8c1ae5e454169d0375fe2a29b58c0bd07fc
takaman <takaman@gmail.com>
2022-05-16 22:15:26 +0300

    Update entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -33,7 +33,7 @@ git remote -v
 git remote update
 
 for branch in ${HEAD_BRANCHES}; do
-git rebase --autosquash --autostash -s \
+git rebase \
 	"origin/${BASE_REF}" "origin/${branch}"
 git push --force origin "HEAD:${branch}"
 done
