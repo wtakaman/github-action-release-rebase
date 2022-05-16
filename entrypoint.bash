159f0196899a23fab4f3d160f78473cbf07f440a
takaman <takaman@gmail.com>
2022-05-16 23:10:31 +0300

    Update entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -46,7 +46,6 @@ git rebase \
 	origin/${BASE_REF}
 echo "pushing ${branch}" 
 
-git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git remote set-url origin "https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git push --force "origin/${branch}"
